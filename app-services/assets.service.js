c1c812c15993b1f0aad3dd65649e1e2b90796a47
Laurent Salou <laurent.salou@hotmail.fr>
2018-10-30 14:26:58 +0800

    Add TILPLR and PANDO logos

--- app-services/assets.service.js
+++ app-services/assets.service.js
@@ -9,7 +9,7 @@
     function Assets() {
         var service = {};
 
-        service.hasIcon = ['ETP', 'MVS.ZGC', 'MVS.ZDC', 'CSD.CSD', 'PARCELX.GPX', 'PARCELX.TEST', 'SDG', 'META', 'MVS.HUG', 'RIGHTBTC.RT'];
+        service.hasIcon = ['ETP', 'MVS.ZGC', 'MVS.ZDC', 'CSD.CSD', 'PARCELX.GPX', 'PARCELX.TEST', 'SDG', 'META', 'MVS.HUG', 'RIGHTBTC.RT', 'TIPLR.TPC', 'PANDO'];
 
         setPriority();
 
@@ -20,7 +20,6 @@
             service.priority["RIGHTBTC.RT"] = 20;
             service.priority["MVS.ZGC"] = 100;
             service.priority["MVS.ZDC"] = 110;
-            service.priority["CSD.CSD"] = 200;
         };
 
         setPriority();
