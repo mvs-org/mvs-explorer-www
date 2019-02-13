0a345ab034ae9d9b6410b883418ba934cd9fa883
canguruhh <sven@mutzl.org>
2019-02-13 18:06:37 +0000

    Set caching headers

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ const botUserAgents = [
 
 const app = express()
 
+app.use(function (req, res, next) {
+    res.set('Cache-Control', 'public, max-age=300, s-max-age=3600')
+    res.set('Vary', 'User-Agent')
+    next()
+})
+
 if (process.env.RENDERTRON_PROXY) {
     app.use(rendertron.makeMiddleware({
         proxyUrl: process.env.RENDERTRON_PROXY,
@@ -31,12 +37,6 @@ if (process.env.RENDERTRON_PROXY) {
     }))
 }
 
-app.use(function (req, res, next) {
-    res.set('Cache-Control', 'public, max-age=300, s-max-age=3600')
-    res.set('Vary', 'User-Agent')
-    next()
-})
-
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist', {setHeaders}));
 
 app.listen(80)
