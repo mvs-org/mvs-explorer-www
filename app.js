80367afcd1d3e3ad7f2e9f3bfa9f50d2c11dbeb8
canguruhh <sven@mutzl.org>
2019-03-15 13:12:25 +0100

    Meta data for SEO

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-(function() {
+(function () {
     'use strict';
 
-    angular.module('app', ['ui.router', 'ngCookies', 'LocalStorageModule', 'pascalprecht.translate', 'angularUtils.directives.dirPagination', 'ngAnimate','ngAria','ngMessages','ngMaterial', 'nvd3', 'vxWamp','ngSanitize','swaggerUi','infinite-scroll'])
+    angular.module('app', ['ui.router', 'ngCookies', 'LocalStorageModule', 'pascalprecht.translate', 'angularUtils.directives.dirPagination', 'ngAnimate', 'ngAria', 'ngMessages', 'ngMaterial', 'nvd3', 'vxWamp', 'ngSanitize', 'swaggerUi', 'infinite-scroll'])
         .config(config)
-        .config(['$compileProvider', function($compileProvider) {
+        .config(['$compileProvider', function ($compileProvider) {
             $compileProvider.debugInfoEnabled(false);
         }])
-        .config(function(localStorageServiceProvider) {
+        .config(function (localStorageServiceProvider) {
             localStorageServiceProvider
                 .setPrefix('mvs.explorer');
-        }).config(function($translateProvider) {
+        }).config(function ($translateProvider) {
             $translateProvider.useStaticFilesLoader({
                 prefix: 'lang/',
                 suffix: '.json'
@@ -22,45 +22,55 @@
             });
             $translateProvider.useSanitizeValueStrategy('escapeParameters');
             var lang = (navigator.languages ? navigator.languages[0] : (navigator.language || navigator.userLanguage));
-            if(lang.indexOf('zh') == 0) {
-              $translateProvider.preferredLanguage('zh-ZH');
+            if (lang.indexOf('zh') == 0) {
+                $translateProvider.preferredLanguage('zh-ZH');
             } else {
-              $translateProvider.preferredLanguage('en-US');
+                $translateProvider.preferredLanguage('en-US');
             }
-        }).config(function($wampProvider) {
+        }).config(function ($wampProvider) {
             $wampProvider.init({
-              //url: 'wss://explorer.mvs.org/ws',
-              //url: 'ws://explorer-dev.mvs.org/ws',
-              //url: 'wss://explorer-testnet.mvs.org/ws',
-              //url: 'ws://localhost:80/ws',
-              url: ((window.location.protocol == 'https:') ? 'wss:' : 'ws:') + "//" + window.location.hostname + ((window.location.port) ? ":" + window.location.port : "") + "/ws",
+                url: 'wss://explorer.mvs.org/ws',
+                //url: 'ws://explorer-dev.mvs.org/ws',
+                //url: 'wss://explorer-testnet.mvs.org/ws',
+                //url: 'ws://localhost:80/ws',
+                // url: ((window.location.protocol == 'https:') ? 'wss:' : 'ws:') + "//" + window.location.hostname + ((window.location.port) ? ":" + window.location.port : "") + "/ws",
                 realm: 'realm1'
                 //Any other AutobahnJS options
             });
         })
-        .directive('bsTooltip', function() {
-          return {
-            restrict: 'A',
-            link: function(scope, element, attrs){
-                $(element).hover(function(){
-                    // on mouseenter
-                    $(element).tooltip('show');
-                }, function(){
-                    // on mouseleave
-                    $(element).tooltip('hide');
-                });
-            }
-          };
-        })
-        .filter('assetformat',function(){
-            return function(input, asset_type){
-                if(typeof asset_type === 'undefined')
-                    asset_type=8;
-                return parseFloat(input)/Math.pow(10,asset_type);
+        .directive('bsTooltip', function () {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    $(element).hover(function () {
+                        // on mouseenter
+                        $(element).tooltip('show');
+                    }, function () {
+                        // on mouseleave
+                        $(element).tooltip('hide');
+                    });
+                }
             };
         })
-        .filter('numberFormat', () => function(number, decimals) {
-            var parts = ((number / Math.pow(10, decimals)).toFixed(decimals)*1).toString().split(".");
+        .service('MetadataService', ['$window', function ($window) {
+            var self = this;
+            self.setMetaTags = function (tagData) {
+                $window.document.getElementsByName('title')[0].content = tagData.title || "Metaverse Blockchain Explorer";
+                $window.document.getElementsByTagName('title')[0].innerHTML = tagData.title || "Metaverse Blockchain Explorer";
+                $window.document.getElementsByTagName('title')[0].innerHTML = tagData.title || "Metaverse Blockchain Explorer";
+                $window.document.getElementsByName('description')[0].content = tagData.description || "Metaverse Blockchain Explorer is a web tool that provides detailed information about Metaverse Smart Assets, Blocks, Addresses, and Transactions.";
+                $window.document.getElementsByName('keywords')[0].content = tagData.keywords || "metaverse, explorer, blockchain, digital identity, asset";
+            };
+        }])
+        .filter('assetformat', function () {
+            return function (input, asset_type) {
+                if (typeof asset_type === 'undefined')
+                    asset_type = 8;
+                return parseFloat(input) / Math.pow(10, asset_type);
+            };
+        })
+        .filter('numberFormat', () => function (number, decimals) {
+            var parts = ((number / Math.pow(10, decimals)).toFixed(decimals) * 1).toString().split(".");
             parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
             return parts.join(".");
         })
@@ -77,11 +87,21 @@
             })
             .state('explorer.startpage', {
                 url: '/',
+                meta: {
+                    title: 'Metaverse Blockchain Explorer',
+                    description: 'The Metaverse Blockchain Explorer provides insights into digital identity, smart assets, transactions and mining.',
+                    keywords: "metaverse, blockchain, explorer"
+                },
                 templateUrl: "views/startpage.view.html",
                 controller: 'StartpageController'
             })
             .state('explorer.api', {
                 url: "/dev/api",
+                meta: {
+                    title: 'Metaverse Explorer API',
+                    description: 'API documentation of the Metaverse Blockchain Explorer. Join our developer community and create your own project today.',
+                    keywords: "metaverse, blockchain, api, developer"
+                },
                 templateUrl: "views/api/api.html"
             })
             .state('explorer.transaction', {
@@ -91,23 +111,33 @@
             })
             .state('explorer.blocks', {
                 url: "/blocks",
+                meta: {
+                    title: 'Metaverse Blocks',
+                    description: 'List all blocks of the Metaverse Blockchain.',
+                    keywords: "metaverse, blockchain, block"
+                },
                 templateUrl: "views/blocks.view.html",
                 controller: 'BlocksController'
             })
             .state('explorer.txs', {
                 url: "/txs",
+                meta: {
+                    title: 'Metaverse Transactions',
+                    description: 'List all transactions of the Metaverse Blockchain.',
+                    keywords: "metaverse, blockchain, transactions"
+                },
                 templateUrl: "views/txs.view.html",
                 controller: 'TransactionsController'
             })
             .state('explorer.mining', {
                 url: "/mining",
+                meta: {
+                    title: 'Metaverse Mining',
+                    description: 'Minning statistics and information of the Metaverse Blockchain. POW and POS mining.',
+                    keywords: "metaverse, blockchain, mining, pow, pos, stake"
+                },
                 templateUrl: "views/mining.view.html",
                 controller: 'MiningController'
-            })
-            .state('explorer.nodemap', {
-                url: "/nodemap",
-                templateUrl: "views/nodemap.view.html",
-                controller: 'NodeMapController'
             })
             .state('explorer.block', {
                 url: "/blk/:number",
@@ -126,6 +156,11 @@
             })
             .state('explorer.avatars', {
                 url: "/avatars",
+                meta: {
+                    title: 'Metaverse Digital Identity - Avatars',
+                    description: 'Register of the Metaverse Blockchain Avatars. Create your own digital identity.',
+                    keywords: "metaverse, blockchain, digital, identity, avatar"
+                },
                 templateUrl: "views/avatars.view.html",
                 controller: 'AvatarsController'
             })
@@ -141,6 +176,11 @@
             })
             .state('explorer.mstlist', {
                 url: "/msts",
+                meta: {
+                    title: 'Metaverse Smart Token - MST',
+                    description: 'Register of the Metaverse Blockchain MST. Create your own smart asset.',
+                    keywords: "metaverse, blockchain, mst, smart, asset"
+                },
                 templateUrl: "views/assetslist.view.html",
                 controller: 'AssetsListController'
             })
@@ -156,6 +196,11 @@
             })
             .state('explorer.mits', {
                 url: "/mits",
+                meta: {
+                    title: 'Metaverse Identifiable Token - MIT',
+                    description: 'Register of the Metaverse Blockchain MIT. Create your own unique identifiable token.',
+                    keywords: "metaverse, blockchain, mit, smart, asset"
+                },
                 templateUrl: "views/mits.view.html",
                 controller: 'MitsController'
             })
@@ -166,6 +211,11 @@
             })
             .state('explorer.news', {
                 url: "/news",
+                meta: {
+                    title: 'Metaverse Blockchain News',
+                    description: 'Latest news and information about the Metaverse Blockchain Ecosystem.',
+                    keywords: "metaverse, blockchain, news"
+                },
                 templateUrl: "views/news.view.html",
                 controller: 'NewsController'
             });
@@ -174,19 +224,22 @@
 
     };
 
-    run.$inject = ['$rootScope', '$location', 'localStorageService', '$translate', '$wamp'];
+    run.$inject = ['$rootScope', '$location', 'localStorageService', '$translate', '$wamp', '$state', 'MetadataService'];
 
-    function run($rootScope, $location, $localStorageService, $translate, $wamp) {
+    function run($rootScope, $location, $localStorageService, $translate, $wamp, $state, MetadataService) {
 
         $wamp.open();
 
-        $rootScope.$on('$stateChangeStart', ()=>$(this).scrollTop(0));
+        $rootScope.$on('$stateChangeSuccess', () => {
+            $(this).scrollTop(0)
+            MetadataService.setMetaTags($state.current.meta || {})
+        });
 
 
         if ($localStorageService.get('language') != undefined)
             $translate.use($localStorageService.get('language'));
 
-        $rootScope.nosplash = false;
+        //$rootScope.nosplash = false;
     }
 
 })();
